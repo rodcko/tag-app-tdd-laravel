584e569d3da799f598d60edd0185e654cc7c0fed
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-04-01 18:21:05 -0600

    Anadiendo estilos con Tailwind CSS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,36 +5,39 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
     </head>
-    <body>
-        <form action="tags" method="POST">
-            @csrf
-            <input type="text" name="name">
-            <input type="submit" value="Agregar">
-        </form>
+    <body class="bg-gray-200 py-10">
+        <div class="max-w-lg bg-white mx-auto p-5 rounded shadow">
+            <form action="tags" method="POST" class="flex mb-4">
+                @csrf
+                <input type="text" name="name" class="rounded-l bg-gray-200 p-4 w-full outline-none" placeholder="Nueva etiqueta">
+                <input type="submit" value="Agregar" class="rounded-r px-8 bg-blue-500 text-white outline-none">
+            </form>
 
-        <h4>Listado de Etiquetas</h4>
+            <h4 class="text-lg text-center mb-4">Listado de Etiquetas</h4>
 
-        <table>
-            @forelse($tags as $tag)
-            <tr>
-                <td>{{ $tag->name }}</td>
-                <td>
-                    <form action="tags/{{ $tag->id }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <input type="submit" value="Eliminar">
-                    </form>
-                </td>
-            </tr>
-            @empty
-            <tr>
-                <td>
-                    <p>No hay etiquetas</p>
-                </td>
-            </tr>
-            @endforelse
-        </table>
+            <table>
+                @forelse($tags as $tag)
+                <tr>
+                    <td class="border px-4 py-2">{{ $tag->name }}</td>
+                    <td class="px-4 py-2">
+                        <form action="tags/{{ $tag->id }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <input type="submit" value="Eliminar" class="px-3 rounded bg-red-500 text-white">
+                        </form>
+                    </td>
+                </tr>
+                @empty
+                <tr>
+                    <td>
+                        <p>No hay etiquetas</p>
+                    </td>
+                </tr>
+                @endforelse
+            </table>
+        </div>
     </body>
 </html>
