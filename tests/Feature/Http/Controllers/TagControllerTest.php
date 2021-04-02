6defd370f1eea8e0f7618653a33463a803d6c022
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-04-01 18:06:55 -0600

    Validacion

--- tests/Feature/Http/Controllers/TagControllerTest.php
+++ tests/Feature/Http/Controllers/TagControllerTest.php
@@ -32,4 +32,11 @@ class TagControllerTest extends TestCase
 
         $this->assertDatabaseMissing('tags', ['name' => $tag->name]);        
     }
+
+    public function testValidate()
+    {
+        $this
+            ->post('tags', ['name' => ''])
+            ->assertSessionHasErrors('name');
+    }
 }
